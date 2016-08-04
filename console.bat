b423a58761ab9205f0e122a3a16069844c033d4e
Francesco Bianco <info.francescobianco@gmail.com>
2016-08-04 17:32:53 +0200

    added edit command

--- console.bat
+++ console.bat
@@ -6,6 +6,7 @@ if "%1" == "__init__" (
 	doskey clear=cls
 	doskey ls=dir /a
 	doskey console=%~dpf0 
+	doskey edit=%~dpf0 edit $1
 	cd %~dp0
 	goto :exit
 )
@@ -34,8 +35,17 @@ if "%1" == "install" (
 	rem del CreateShortcut.vbs
 	echo console.bat installed on %2
 	echo use Desktop shortcup to launch
-	goto :exit
+	goto:eof
 )
+
+rem edit command
+if "%1" == "edit" (
+	start /B "" "%CONSOLE_EDIT%" %2
+	goto:eof
+)
+
+rem detect edit command
+set CONSOLE_EDIT=%ProgramFiles(x86)%\Notepad++\notepad++.exe
 
 rem prepare dos prompt
 set PROMP0=%PROMPT%
